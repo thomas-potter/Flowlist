59de848105de32cd244502f15203715944dfee5c
Thomas <68824290+thomas-potter@users.noreply.github.com>
2025-04-07 13:49:55 +0100

    Updating with analtics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import { type Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 // Import Poppins font
 const poppins = Poppins({
@@ -28,6 +30,8 @@ export default function RootLayout({
       </head>
       <body>
         {/* Header */}
+        <SpeedInsights />
+        <Analytics />
         <header className="header">
           <div className="container">
           </div>
